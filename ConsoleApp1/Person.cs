006ed8b704c6bcb4bea7e6ffc2f8e5f0687ca747
Nguyễn Trường Dũng <nguyendung87neol@hotmail.com>
2017-07-18 23:11:01 +0700

    dsafsa

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Old { get; set; }
+    }
+}
